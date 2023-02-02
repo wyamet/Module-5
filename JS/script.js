c5b9b34877edfb42d207ba54ab3f8a6b19283131
wyamet <wyattdomanski.md@gmail.com>
2023-02-02 11:10:42 -0800

    fixing error of current color not showing

--- JS/script.js
+++ JS/script.js
@@ -2,64 +2,61 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-  
+  //grab dayjs current time and assign to variable
 
+  var rightNow = dayjs().format("MMM:DD:YYYY ");
+  $("#currentTime").text(rightNow);
 
-//grab dayjs current time and assign to variable
+  // setInterval(rightNow, 10000);
+  // function hourUpdate(){
+  // var currentHour = dayjs().hour();
 
-var rightNow = dayjs().format('MMM:DD:YYYY ');
-$("#currentTime").text(rightNow);
+  // change the color of the block to match the time display
+  function blockColor() {
+    var timeCurrent = dayjs().hour();
 
+    // parse the id attribute into an integer that compares with the dayjs hour.
+    $(".time-block").each(function () {
+      var currentBlock = parseInt($(this).attr("id").split("-")[1]);
+      if (currentBlock < timeCurrent) {
+        // $(this).removeClass("present");
+        // $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (currentBlock === timeCurrent) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
 
-// setInterval(rightNow, 10000);
-
-
-
-// change the color of the block to match the time display
-function blockColor(){
-  var timeCurrent = dayjs().hour();
-  // parse the id attribute into an integer that compares with the dayjs hour.
-  $(".time-block").each(function(){
-    
-    var currentBlock = parseInt($(this).attr("id").split("-")[1])
-    if (currentBlock < timeCurrent) { 
-      $(this).removeClass("present");
-      $(this).removeClass("future");
-      $(this).addClass("past");
-    } else if (currentBlock === timeCurrent) {
-      $(this).removeClass("future");
-      $(this).removeClass("past");
-      $(this).addClass("present");
-    } else (currentBlock > timeCurrent) 
-      $(this).removeClass("past");
-      $(this).removeClass("present");
-      $(this).addClass("future")
-    })
-  };
   blockColor();
 
-// save to localStorage on the click of the save button
-$(function () {
-  $('.saveBtn').on('click', function (){
-    console.log($(this))
-    var hour = $(this).parent().attr("id");
-    var description = $(this).siblings('.description').val();
-    localStorage.setItem(hour, description);
-    
+  // save to localStorage on the click of the save button
+  $(function () {
+    $(".saveBtn").on("click", function () {
+      console.log($(this));
+      var hour = $(this).parent().attr("id");
+      var description = $(this).siblings(".description").val();
+      localStorage.setItem(hour, description);
+    });
   });
+  //grab element data from local storage.
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
-//grab element data from local storage.
-$("#hour-8 .description").val(localStorage.getItem("hour-8"))
-$("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-10"))
-$("#hour-11 .description").val(localStorage.getItem("hour-11"))
-$("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-13 .description").val(localStorage.getItem("hour-13"))
-$("#hour-14 .description").val(localStorage.getItem("hour-14"))
-$("#hour-15 .description").val(localStorage.getItem("hour-15"))
-$("#hour-16 .description").val(localStorage.getItem("hour-16"))
-$("#hour-17 .description").val(localStorage.getItem("hour-17"))
-
-});
-
